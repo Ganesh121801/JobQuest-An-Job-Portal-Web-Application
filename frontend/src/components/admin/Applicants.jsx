69aeacb7ae3e887332d288f94e5171a27da125d9
Ganesh More <136815319+Ganesh121801@users.noreply.github.com>
2025-01-09 17:34:45 +0530

    Update Applicants.jsx

--- frontend/src/components/admin/Applicants.jsx
+++ frontend/src/components/admin/Applicants.jsx
@@ -1,42 +1,42 @@
-import React, { useEffect } from 'react'
-import Navbar from '../shared/Navbar'
-import ApplicantsTable from './ApplicantsTable'
-import axios from 'axios'
-import { useParams } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { setAllApplicants } from '@/redux/applicationSlice'
-import { APPLICATION_API_END_POINT } from '@/utils/constant'
+import React, { useEffect } from 'react';
+import Navbar from '../shared/Navbar';
+import ApplicantsTable from './ApplicantsTable';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { setAllApplicants } from '@/redux/applicationSlice';
+import { APPLICATION_API_END_POINT } from '@/utils/constant';
 
 const Applicants = () => {
-
-    //how to get id  
-    const params = useParams();
+    const params = useParams(); // To get the job ID from the URL
     const dispatch = useDispatch();
-  const {applicants} = useSelector(store => store.application)
-     
+    const { applicants } = useSelector(store => store.application);
 
     useEffect(() => {
         const fetchAllApplicants = async () => {
             try {
                 const res = await axios.get(`${APPLICATION_API_END_POINT}/${params.id}/applicants`, { withCredentials: true });
-                
-                dispatch(setAllApplicants(res.data.job))
+                dispatch(setAllApplicants(res.data.job));
             } catch (error) {
-                console.log(error);
-
+                console.error(error);
             }
-        }
+        };
         fetchAllApplicants();
-    }, [])
+    }, [params.id, dispatch]);
+
     return (
         <div>
             <Navbar />
-            <div className='max-w-7xl mx-auto'>
-                <h1 className='font-bold text-xl my-5'>Applicants {applicants?.applications?.length}</h1>
-                <ApplicantsTable />
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <h1 className="font-bold text-lg sm:text-xl lg:text-2xl my-5 text-center sm:text-left">
+                    Applicants ({applicants?.applications?.length})
+                </h1>
+                <div className="overflow-x-auto">
+                    <ApplicantsTable />
+                </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Applicants
+export default Applicants;
