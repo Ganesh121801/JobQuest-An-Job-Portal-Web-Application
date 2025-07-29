8d4e09cf861c978ec8f4850c2963d1cd6fa5b38b
Ganesh More <136815319+Ganesh121801@users.noreply.github.com>
2025-07-29 20:58:49 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,6 +1,4 @@
-export const USER_API_ENDPOINT = "https://jobquest-an-job-portal-application.onrender.com/api/v1/user";
-export const JOB_API_END_POINT = "https://jobquest-an-job-portal-application.onrender.com/api/v1/job";
-export const APPLICATION_API_END_POINT="https://jobquest-an-job-portal-application.onrender.com/api/v1/application";
-export const COMPANY_API_END_POINT="https://jobquest-an-job-portal-application.onrender.com/api/v1/company";
-
-
+export const USER_API_ENDPOINT = import.meta.env.VITE_USER_API_ENDPOINT;
+export const JOB_API_END_POINT = import.meta.env.VITE_JOB_API_END_POINT;
+export const APPLICATION_API_END_POINT = import.meta.env.VITE_APPLICATION_API_END_POINT;
+export const COMPANY_API_END_POINT = import.meta.env.VITE_COMPANY_API_END_POINT;
